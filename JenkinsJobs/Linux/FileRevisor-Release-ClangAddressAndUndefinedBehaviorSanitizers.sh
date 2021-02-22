e83adad301b39fe2db5421d3f8f3c927fdbd2ae5
Neil Justice <njjustice@gmail.com>
2021-02-21 23:35:21 -0600

    Update Jenkins job script JenkinsJobs/Linux/FileRevisor-Release-ClangAddressAndUndefinedBehaviorSanitizers.sh

--- JenkinsJobs/Linux/FileRevisor-Release-ClangAddressAndUndefinedBehaviorSanitizers.sh
+++ JenkinsJobs/Linux/FileRevisor-Release-ClangAddressAndUndefinedBehaviorSanitizers.sh
@@ -3,6 +3,7 @@ set -eu
 
 export CXX=clang++
 export UBSAN_OPTIONS=halt_on_error=true
+export PYTHONPATH=FileRevisorPyUtils
 python -u FileRevisorPyUtils/FileRevisorPyUtils/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-generator=Ninja \
