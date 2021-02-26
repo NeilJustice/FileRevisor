645db3bc587dac9194659a484c68a5586331ccdd
Neil Justice <njjustice@gmail.com>
2021-02-25 23:30:58 -0600

    Update Linux Jenkins scripts

--- JenkinsJobs/Linux/FileRevisor-Release-ClangAddressSanitizer.sh
+++ JenkinsJobs/Linux/FileRevisor-Release-ClangAddressSanitizer.sh
@@ -2,12 +2,11 @@
 set -eu
 
 export CXX=clang++
-export UBSAN_OPTIONS=halt_on_error=true
 export PYTHONPATH=FileRevisorPyUtils
 python -u FileRevisorPyUtils/FileRevisorPyUtils/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-generator=Ninja \
    --cmake-build-type=Release \
    --tests-project-name=libFileRevisorTests \
-   --cmake-definitions="-DClangSanitizersMode_AddressAndUndefined=ON" \
+   --cmake-definitions="-DClangAddressSanitizerMode=ON" \
    --no-install
--- JenkinsJobs/Linux/FileRevisor-Release-ClangThreadSanitizer.sh
+++ JenkinsJobs/Linux/FileRevisor-Release-ClangThreadSanitizer.sh
@@ -2,12 +2,11 @@
 set -eu
 
 export CXX=clang++
-export UBSAN_OPTIONS=halt_on_error=true
 export PYTHONPATH=FileRevisorPyUtils
 python -u FileRevisorPyUtils/FileRevisorPyUtils/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-generator=Ninja \
    --cmake-build-type=Release \
    --tests-project-name=libFileRevisorTests \
-   --cmake-definitions="-DClangSanitizersMode_Thread=ON" \
+   --cmake-definitions="-DClangThreadSanitizerMode=ON" \
    --no-install
