9c58b36237b392ae4a0add880bec6cac9412019a
Neil Justice <njjustice@gmail.com>
2021-02-27 16:27:57 -0600

    Add 'export UBSAN_OPTIONS=halt_on_error=true' to JenkinsJobs/Linux/FileRevisor-Release-ClangUndefinedBehaviorSanitizer.sh

--- JenkinsJobs/Linux/FileRevisor-Release-ClangUndefinedBehaviorSanitizer.sh
+++ JenkinsJobs/Linux/FileRevisor-Release-ClangUndefinedBehaviorSanitizer.sh
@@ -3,6 +3,7 @@ set -eu
 
 export CXX=clang++
 export PYTHONPATH=FileRevisorPyUtils
+export UBSAN_OPTIONS=halt_on_error=true
 python -u FileRevisorPyUtils/FileRevisorPyUtils/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-generator=Ninja \
