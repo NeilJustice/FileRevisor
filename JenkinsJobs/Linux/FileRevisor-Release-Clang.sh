3ae15aa4a6c25f68de34464c3e9a55ee60005bb3
Neil Justice <njjustice@gmail.com>
2021-03-15 19:37:56 -0500

    Add 'export PYTHONPATH=FileRevisorPyUtils' to JenkinsJobs/Linux/FileRevisor-Release-Clang.sh

--- JenkinsJobs/Linux/FileRevisor-Release-Clang.sh
+++ JenkinsJobs/Linux/FileRevisor-Release-Clang.sh
@@ -2,6 +2,7 @@
 set -ev
 
 export CXX=clang++
+export PYTHONPATH=FileRevisorPyUtils
 python -u FileRevisorPyUtils/FileRevisorPyUtils/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-generator=Ninja \
