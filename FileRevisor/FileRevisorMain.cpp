565b1ec60ef60743d4d44847bcea5a1f35752ce4
Neil Justice <njjustice@gmail.com>
2021-04-03 20:43:25 -0500

    cbegin(elements) to elements.cbegin()

--- FileRevisor/FileRevisorMain.cpp
+++ FileRevisor/FileRevisorMain.cpp
@@ -2,7 +2,7 @@
 #include "libFileRevisor/Components/FileRevisor/FileRevisorProgram.h"
 
 // Example FileRevisor command line arguments:
-// delete-directory --target=C:\FileRevisorTesting\Folder --parallel
+// delete-directory --target=C:\FileRevisorTesting --parallel
 // rename-files --target=C:\FileRevisorTesting\Folder --from=from --to=to --recurse --preview --verbose
 // rename-directories --target=C:\FileRevisorTesting\Folder --from=from --to=to --recurse --preview --verbose
 // replace-text --target=C:\FileRevisorTesting\Folder --from=from --to=to --recurse --preview --verbose
--- libFileRevisor/Components/Iteration/ForEach/ParallelOneExtraArgMemberForEacher.h
+++ libFileRevisor/Components/Iteration/ForEach/ParallelOneExtraArgMemberForEacher.h
@@ -7,7 +7,7 @@ public:
    virtual ~ParallelOneExtraArgMemberForEacher() = default;
 
    virtual void ParallelOneExtraArgMemberForEach(
-      const std::vector<T>& elements,
+      const vector<T>& elements,
       const ClassType* constClassPointer,
       MemberFunctionType constMemberFunction,
       ExtraArgType extraArg) const
@@ -17,11 +17,11 @@ public:
       __gnu_parallel::_Settings settings;
       settings.algorithm_strategy = __gnu_parallel::force_parallel;
       __gnu_parallel::_Settings::set(settings);
-      __gnu_parallel::for_each(cbegin(elements), cend(elements), extraArgBoundMemberFunction);
+      __gnu_parallel::for_each(elements.cbegin(), elements.cend(), extraArgBoundMemberFunction);
 #elif defined _WIN32
-      std::for_each(std::execution::par_unseq, cbegin(elements), cend(elements), extraArgBoundMemberFunction);
+      std::for_each(std::execution::par_unseq, elements.cbegin(), elements.cend(), extraArgBoundMemberFunction);
 #else
-      std::for_each(cbegin(elements), cend(elements), extraArgBoundMemberFunction);
+      std::for_each(elements.cbegin(), elements.cend(), extraArgBoundMemberFunction);
 #endif
    }
 };
