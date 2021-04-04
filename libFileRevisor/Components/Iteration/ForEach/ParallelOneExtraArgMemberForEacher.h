a95dbd966cc2a5b2af48d43880e1360ae432c4c0
Neil Justice <njjustice@gmail.com>
2021-04-03 20:35:16 -0500

    Remove uneeded parens around #ifdef values in libFileRevisor/Components/Iteration/ForEach/ParallelOneExtraArgMemberForEacher.h

--- libFileRevisor/Components/Iteration/ForEach/ParallelOneExtraArgMemberForEacher.h
+++ libFileRevisor/Components/Iteration/ForEach/ParallelOneExtraArgMemberForEacher.h
@@ -13,12 +13,12 @@ public:
       ExtraArgType extraArg) const
    {
       const auto extraArgBoundMemberFunction = std::bind(constMemberFunction, constClassPointer, std::placeholders::_1, extraArg);
-#if defined(__GNUG__) && !defined(__clang__)
+#if defined __GNUG__ && !defined __clang__
       __gnu_parallel::_Settings settings;
       settings.algorithm_strategy = __gnu_parallel::force_parallel;
       __gnu_parallel::_Settings::set(settings);
       __gnu_parallel::for_each(cbegin(elements), cend(elements), extraArgBoundMemberFunction);
-#elif defined(_WIN32)
+#elif defined _WIN32
       std::for_each(std::execution::par_unseq, cbegin(elements), cend(elements), extraArgBoundMemberFunction);
 #else
       std::for_each(cbegin(elements), cend(elements), extraArgBoundMemberFunction);
