8e3edcfda3b9c531f80f70749023753fb35380c3
Neil Justice <njjustice@gmail.com>
2021-06-04 20:38:20 -0500

    Add CMake variable FastWindowsDebugBuildMode to optionally build without symbols for faster build speed

--- JenkinsJobs/Windows/FileRevisor-Debug.ps1
+++ JenkinsJobs/Windows/FileRevisor-Debug.ps1
@@ -6,6 +6,7 @@ python.exe -u FileRevisorDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --cmake-generator="Visual Studio 16 2019" `
    --cmake-build-type=Debug `
    --tests-project=libFileRevisorTests `
+   --cmake-definitions="-DFastWindowsDebugBuildMode=ON" `
    --no-install
 
 Set-PSDebug -Trace 0
