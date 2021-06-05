5b1a849f19bf085dba3947d7cddcc0e7072f335e
Neil Justice <njjustice@gmail.com>
2021-06-05 09:35:54 -0500

    In githooks/pre-commit and Jenkins job script JenkinsJobs/Windows/FileRevisor-Debug.ps1, only enable -DFastWindowsDebugBuildMode=ON when invoked by Jenkins and not the precommit Git script

--- JenkinsJobs/Windows/FileRevisor-Debug.ps1
+++ JenkinsJobs/Windows/FileRevisor-Debug.ps1
@@ -1,3 +1,5 @@
+param($fastWindowsDebugBuildMode = "ON")
+
 Set-PSDebug -Trace 1
 
 $env:PYTHONPATH="."
@@ -6,8 +8,8 @@ python.exe -u FileRevisorDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --cmake-generator="Visual Studio 16 2019" `
    --cmake-build-type=Debug `
    --tests-project=libFileRevisorTests `
-   --cmake-definitions="-DFastWindowsDebugBuildMode=ON" `
+   --cmake-definitions="-DFastWindowsDebugBuildMode=$fastWindowsDebugBuildMode" `
    --no-install
-
+   
 Set-PSDebug -Trace 0
 exit $LastExitCode
