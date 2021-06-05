0630e580123878cc27a728adc48903a1d5ffc7bb
Neil Justice <njjustice@gmail.com>
2021-06-05 12:55:42 -0500

    Replace 'scoped_lock<mutex> coutLock(_coutMutex)' with 'std::osyncstream{cout}' in libFileRevisor/Components/Console/Console.cpp

--- libFileRevisor/Components/Console/Console.cpp
+++ libFileRevisor/Components/Console/Console.cpp
@@ -1,8 +1,8 @@
 #include "pch.h"
 #include "libFileRevisor/Components/Console/Console.h"
+#include <syncstream>
 
 void Console::WriteLine(string_view message) const
 {
-   scoped_lock<mutex> coutLock(_coutMutex);
-   cout << message << '\n';
+   std::osyncstream{cout} << message << '\n';
 }
