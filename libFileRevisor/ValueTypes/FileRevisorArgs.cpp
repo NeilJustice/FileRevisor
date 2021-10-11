aae9669634c839ba383a453cc794055231cba99d
Neil Justice <njjustice@gmail.com>
2021-10-11 18:17:55 -0500

    Increase version to v1.1.0

--- libFileRevisor/ValueTypes/FileRevisorArgs.cpp
+++ libFileRevisor/ValueTypes/FileRevisorArgs.cpp
@@ -2,7 +2,7 @@
 #include "libFileRevisor/ValueTypes/FileRevisorArgs.h"
 
 const string FileRevisorArgs::CommandLineUsage =
-R"(FileRevisor v1.0.0
+R"(FileRevisor v1.1.0
 Quickly deletes directories, renames files, renames directories, or replaces text in files.
 
 Usage:
