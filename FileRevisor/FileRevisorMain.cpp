4758b627837f5a163bfcd59afd33473bbd053c93
Neil Justice <njjustice@gmail.com>
2022-01-08 09:30:32 -0600

    Remove testing code from FileRevisor/FileRevisorMain.cpp

--- FileRevisor/FileRevisorMain.cpp
+++ FileRevisor/FileRevisorMain.cpp
@@ -7,12 +7,8 @@
 // rename-directories --target=C:\FileRevisorTesting --from=dir1 --to=new_dir_name --dryrun --verbose
 // replace-text --target=D:\FileRevisorTesting --from=100 --to=200 --recurse --verbose
 
-#include <fstream>
-
 int main(int argc, char* argv[])
 {
-   std::ofstream f(R"(D:\FileRevisorTesting\a.txt)");
-
    FileRevisorProgram fileRevisorProgram;
    const int exitCode = fileRevisorProgram.Main(argc, argv);
    return exitCode;
