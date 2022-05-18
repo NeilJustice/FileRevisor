fe1c6fe88157e97baf20c10062f0468dd7ec4ab9
Neil Justice <njjustice@gmail.com>
2022-05-18 16:24:33 -0500

    Add ignore of warning 5246 on Windows: 'the initialization of a subobject should be wrapped in braces'

--- libFileRevisor/Compiler/IfWindowsIgnoreTheseWarningsGlobally.h
+++ libFileRevisor/Compiler/IfWindowsIgnoreTheseWarningsGlobally.h
@@ -1,17 +1,18 @@
 #pragma once
 
 #ifdef _WIN32
-   #pragma warning(disable: 4061) // enumerator 'ProgramMode::Unset' in switch of enum 'ProgramMode' is not explicitly handled by a case label
-   #pragma warning(disable: 4514) // 'Function' unreferenced inline function has been removed
-   #pragma warning(disable: 4623) // '__std_type_info_data': default constructor was implicitly defined as deleted
-   #pragma warning(disable: 4625) // copy constructor was implicitly defined as deleted
-   #pragma warning(disable: 4626) // assignment operator was implicitly defined as deleted
-   #pragma warning(disable: 4710) // function not inlined
-   #pragma warning(disable: 4711) // function selected for automatic inline expansion
-   #pragma warning(disable: 4820) // 'TypeName': 'N' bytes padding added after data member 'dataMember'   
-   #pragma warning(disable: 4866) // compiler may not enforce left-to-right evaluation order for call to 'std::operator<<<char,std::char_traits<char> >   
-   #pragma warning(disable: 5026) // move constructor was implicitly defined as deleted
-   #pragma warning(disable: 5027) // move assignment operator was implicitly defined as deleted
-   #pragma warning(disable: 5039) // pointer or reference to potentially throwing function passed to extern C function under -EHc. Undefined behavimay occur if this function throws an exception.
-   #pragma warning(disable: 5045) // Compiler will insert Spectre mitigation for memory load if / Qspectre switch specified
+#pragma warning(disable: 4061) // enumerator 'ProgramMode::Unset' in switch of enum 'ProgramMode' is not explicitly handled by a case label
+#pragma warning(disable: 4514) // 'Function' unreferenced inline function has been removed
+#pragma warning(disable: 4623) // '__std_type_info_data': default constructor was implicitly defined as deleted
+#pragma warning(disable: 4625) // copy constructor was implicitly defined as deleted
+#pragma warning(disable: 4626) // assignment operator was implicitly defined as deleted
+#pragma warning(disable: 4710) // function not inlined
+#pragma warning(disable: 4711) // function selected for automatic inline expansion
+#pragma warning(disable: 4820) // 'TypeName': 'N' bytes padding added after data member 'dataMember'
+#pragma warning(disable: 4866) // compiler may not enforce left-to-right evaluation order for call to 'std::operator<<<char,std::char_traits<char> >
+#pragma warning(disable: 5026) // move constructor was implicitly defined as deleted
+#pragma warning(disable: 5027) // move assignment operator was implicitly defined as deleted
+#pragma warning(disable: 5039) // pointer or reference to potentially throwing function passed to extern C function under -EHc. Undefined behavimay occur if this function throws an exception.
+#pragma warning(disable: 5045) // Compiler will insert Spectre mitigation for memory load if / Qspectre switch specified
+#pragma warning(disable: 5246) // '_Elems': the initialization of a subobject should be wrapped in braces
 #endif
