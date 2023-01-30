8c2c447f8b05e1ce26ba49096e4ced41919defdc
Neil Justice <njjustice@gmail.com>
2023-01-29 20:03:40 -0600

    Add JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1

--- JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1
+++ JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1
@@ -2,8 +2,8 @@ $env:PYTHONPATH="."
 python.exe -u FileRevisorDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --solution-name=FileRevisor `
    --cmake-generator="Visual Studio 17 2022" `
-   --cmake-build-type=Release `
+   --cmake-build-type=RelWithDebInfo `
    --tests-project-name=libFileRevisorTests `
-   --cmake-definitions="-DFastWindowsReleaseBuildMode=ON" `
-   --no-install
+   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=C:\" `
+   --install
 exit $LastExitCode
