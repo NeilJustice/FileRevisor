350af193ef186c1790a25da8c4b09923cc76fce8
Neil Justice <njjustice@gmail.com>
2025-01-11 19:38:36 -0500

    Add JenkinsJobs/Linux/FileRevisor-Debug-Clang-Coverage.sh

---
+++ JenkinsJobs/Linux/FileRevisor-Debug-Clang-Coverage.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ev
+
+export CXX=clang++
+export PYTHONPATH=.
+python -u FileRevisorDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
+   --solution-name=FileRevisor \
+   --cmake-build-type=Debug \
+   --tests-project-name=libFileRevisorTests \
+   --cmake-definitions="-DClangCodeCoverageMode=ON" \
+   --no-install
+
+python /code/CodeCovSidekick/CodeCovSidekick.py --solution=FileRevisor --tests-project=libFileRevisorTests
