88a574e98fd9c2ec13a25b9a7928fe7d9037e89c
Neil Justice <njjustice@gmail.com>
2025-01-23 07:36:41 -0600

    JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1 : Update CMAKE_INSTALL_PREFIX from X:\ to C:\

--- JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1
+++ JenkinsJobs/Windows/FileRevisor-RelWithDebInfo.ps1
@@ -3,6 +3,6 @@ python.exe -u FileRevisorDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --solution-name=FileRevisor `
    --cmake-build-type=RelWithDebInfo `
    --tests-project-name=libFileRevisorTests `
-   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=C:\" `
+   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=X:\" `
    --install
 exit $LastExitCode
