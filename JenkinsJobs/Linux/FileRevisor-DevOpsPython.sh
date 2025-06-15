295a7980be6028c062c367b000f89d08a45ccf86
Neil Justice <njjustice@gmail.com>
2025-06-15 17:25:50 -0500

    Fix JenkinsJobs/Linux/FileRevisor-DevOpsPython.sh

--- JenkinsJobs/Linux/FileRevisor-DevOpsPython.sh
+++ JenkinsJobs/Linux/FileRevisor-DevOpsPython.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 
+cd FileRevisorDevOpsPython
 export PYTHONPATH=.
 python FileRevisorDevOpsPython/MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=FileRevisorDevOpsPythonTests/RunAllWithCoverage.py
+cd ..
