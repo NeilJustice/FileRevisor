b910bd1e2ca26a9dcae85e61dd9a0a736d8078ec
Neil Justice <njjustice@gmail.com>
2025-12-07 10:53:00 -0600

    Implement JenkinsJobs/Windows/FileRevisor-Debug-ReadLLVMCodeCoverageReport.ps1

---
+++ JenkinsJobs/Windows/FileRevisor-Debug-ReadLLVMCodeCoverageReport.ps1
@@ -0,0 +1,2 @@
+LinuxCodeCoverageRunner.exe read-llvm-code-coverage-text-report `
+   --llvm-code-coverage-text-report="C:\CodeCoverageReports\FileRevisor\covhtml\FileRevisor_llvm_code_coverage_report.txt"
