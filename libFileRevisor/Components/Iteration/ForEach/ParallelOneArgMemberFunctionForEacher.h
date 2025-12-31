87d204bf010af0f6cc55a0a5a4fecfd6261fb448
Neil Justice <njjustice@gmail.com>
2025-12-30 20:10:52 -0600

    Replace __gnu_parallel for each loops with std::execution::par_unseq parallel for each loops

--- libFileRevisor/Components/Iteration/ForEach/ParallelOneArgMemberFunctionForEacher.h
+++ libFileRevisor/Components/Iteration/ForEach/ParallelOneArgMemberFunctionForEacher.h
@@ -1,8 +1,4 @@
 #pragma once
-#if defined __GNUG__ && !defined __clang__
-#include <parallel/algorithm>
-#include <parallel/settings.h>
-#endif
 
 template<typename ClassType, typename T>
 class ParallelOneArgMemberFunctionForEacher
@@ -18,15 +14,6 @@ public:
       ConstMemberFunctionType constMemberFunction) const
    {
       const auto boundMemberFunction = std::bind(constMemberFunction, constClassPointer, std::placeholders::_1);
-#if defined __GNUG__ && !defined __clang__
-      __gnu_parallel::_Settings settings;
-      settings.algorithm_strategy = __gnu_parallel::force_parallel;
-      __gnu_parallel::_Settings::set(settings);
-      __gnu_parallel::for_each(elements.cbegin(), elements.cend(), boundMemberFunction);
-#elif defined _WIN32
       std::for_each(std::execution::par_unseq, elements.cbegin(), elements.cend(), boundMemberFunction);
-#else
-      std::for_each(elements.cbegin(), elements.cend(), boundMemberFunction);
-#endif
    }
 };
--- libFileRevisor/Components/Iteration/ForEach/ParallelThreeArgMemberFunctionForEacher.h
+++ libFileRevisor/Components/Iteration/ForEach/ParallelThreeArgMemberFunctionForEacher.h
@@ -1,8 +1,4 @@
 #pragma once
-#if defined __GNUG__ && !defined __clang__
-#include <parallel/algorithm>
-#include <parallel/settings.h>
-#endif
 
 template<typename ClassType, typename T, typename Arg2Type, typename Arg3Type>
 class ParallelThreeArgMemberFunctionForEacher
@@ -20,15 +16,6 @@ public:
       Arg3Type arg3) const
    {
       const auto boundMemberFunction = std::bind(constMemberFunction, constClassPointer, std::placeholders::_1, arg2, arg3);
-#if defined __GNUG__ && !defined __clang__
-      __gnu_parallel::_Settings settings;
-      settings.algorithm_strategy = __gnu_parallel::force_parallel;
-      __gnu_parallel::_Settings::set(settings);
-      __gnu_parallel::for_each(elements.cbegin(), elements.cend(), boundMemberFunction);
-#elif defined _WIN32
       std::for_each(std::execution::par_unseq, elements.cbegin(), elements.cend(), boundMemberFunction);
-#else
-      std::for_each(elements.cbegin(), elements.cend(), boundMemberFunction);
-#endif
    }
 };
