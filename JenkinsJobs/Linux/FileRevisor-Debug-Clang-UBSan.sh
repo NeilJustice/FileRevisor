d815cce7a4ebb211933c87d5dbcc7c1c4f4aa4a2
Neil Justice <njjustice@gmail.com>
2026-07-05 18:30:04 -0500

    Add 'export UBSAN_OPTIONS=silence_unsigned_overflow=1' to JenkinsJobs/Linux/FileRevisor-Debug-Clang-UBSan.sh

--- JenkinsJobs/Linux/FileRevisor-Debug-Clang-UBSan.sh
+++ JenkinsJobs/Linux/FileRevisor-Debug-Clang-UBSan.sh
@@ -3,6 +3,7 @@ set -ev
 
 export CXX=clang++
 export PYTHONPATH=FileRevisorDevOpsPython
+export UBSAN_OPTIONS="silence_unsigned_overflow=1"
 python -u FileRevisorDevOpsPython/FileRevisorDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=FileRevisor \
    --cmake-build-type=Debug \
